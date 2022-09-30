3a212c2fec8c0a642fe542f1ab20c19c8acf8e7e
Lukasz Szulik <lukaszgit@o2.pl>
2022-09-30 20:22:16 +0200

    Improved variable

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const  argButtonName,
+'use strict';
+
+const  
        buttonPaper = document.getElementById('button-paper');
        buttonRock = document.getElementById('button-rock');
        buttonScissors = document.getElementById('button-scissors');
@@ -16,7 +18,7 @@ buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); }
 function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
-  const argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
+  let argComputerMove, argMoveId, argPlayerMove, playerInput, argButtonName;
 
 /**
  * Describe this function...
@@ -56,13 +58,13 @@ function displayResult(argPlayerMove, argComputerMove) {
 
 
 'Wygrywasz!';
-playerMove = argButtonName;
+ const playerMove = argButtonName;
 console.log('wybór ruchu gracza to: ' + playerInput);
 
 console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
+const randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
+const computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
 }
